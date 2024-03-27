e9f8e36340451ddb3ea1a091ed3f6ebb1c610342
francyfox <info@shalotts.site>
2024-03-27 19:38:29 +0600

    feat: naive ui ssr + faker

--- components/header-base/HeaderBase.vue
+++ components/header-base/HeaderBase.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
-
+import { NButton } from 'naive-ui'
 </script>
 
 <template>
   <header class="section section-header">
     <div class="container">
-      <div class="row">
+      <div class="row header-list">
+        <a href="/" class="header-list-logo" title="Вернуться домой">
+          <svgo-icon-logo class="link" />
+        </a>
 
+        <nav class="header-menu">
+          <NuxtLink href="#"
+                    class="header-menu-item"
+          >
+            <n-button>Default</n-button>
+          </NuxtLink>
+        </nav>
       </div>
     </div>
   </header>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "header-base.style";
 </style>
