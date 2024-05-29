1045ac00532634c2699553f97a371093a10052c0
Kirill <kicharr@yandex.ru>
2024-05-29 13:39:30 +0300

    feat: changed the output of the list of needy on the /help page, and changed the command request

--- components/insane-payment-form/insane-payment-form.data.ts
+++ components/insane-payment-form/insane-payment-form.data.ts
@@ -1,27 +1,7 @@
 export const paymentOptions = [
-  {
-    label: 'Банковская карта',
-    value: 'bankCard',
-    icon: '/img/payment-options-images/icon-bank-card.svg',
-  },
-  {
-    label: 'SberPay',
-    value: 'sberPay',
-    icon: '/img/payment-options-images/icon-sber-pay.svg',
-  },
-  {
-    label: 'Яндекс Деньги',
-    value: 'yandexMoney',
-    icon: '/img/payment-options-images/icon-yandex-money.svg',
-  },
-  {
-    label: 'Web Money',
-    value: 'webMoney',
-    icon: '/img/payment-options-images/icon-web-money.svg',
-  },
-  {
-    label: 'Мобильный телефон',
-    value: 'mobilePhone',
-    icon: '/img/payment-options-images/icon-mobile-pay.svg',
-  }
+    {
+        label: 'SberPay',
+        value: 'sberPay',
+        icon: '/img/payment-options-images/icon-sber-pay.svg',
+    }
 ]
--- store/list-need.ts
+++ store/list-need.ts
@@ -1,49 +1,59 @@
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 export const useListNeed = defineStore('listNeed', () => {
-  const { locale } = useI18n()
-  const activeListNeed = ref([])
-  const finalListNeed = ref([])
-  const allListNeed = ref([])
-  const getActiveListNeed = async (page: number, number: number): Promise<any> => useApi('/help', {
-    lazy: true,
-    method: "GET",
-    headers: {
-      'Accept-Language': locale.value,
-    },
-    query: {
-      page,
-      number
-    }
-  })
+    const {locale} = useI18n()
+    const activeListNeed = ref([])
+    const finalListNeed = ref([])
+    const allListNeed = ref([])
+    const getActiveListNeed = async (page: number, number: number): Promise<any> => useApi('/help', {
+        lazy: true,
+        method: "GET",
+        headers: {
+            'Accept-Language': locale.value,
+        },
+        query: {
+            page,
+            number
+        }
+    })
 
-  const getFinalListNeed = async (page: number, number: number): Promise<any> => useApi('/help-end', {
-    lazy: true,
-    method: "GET",
-    headers: {
-      'Accept-Language': locale.value,
-    },
-    query: {
-      page,
-      number,
-    }
-  })
+    const getFinalListNeed = async (page: number, number: number): Promise<any> => useApi('/help-end', {
+        lazy: true,
+        method: "GET",
+        headers: {
+            'Accept-Language': locale.value,
+        },
+        query: {
+            page,
+            number,
+        }
+    })
 
-  const getPersonById = async (id: number): Promise<any> => useApi('/help-one', {
-    method: "GET",
-    headers: {
-      'Accept-Language': locale.value,
-    },
-    query: {
-      id
-    }
-  })
+    const getAllListNeed = async (): Promise<any> => {
+        const {data} = await useApi('/help-main', {
+            method: "GET"
+        });
 
-  return {
-    getPersonById,
-    getActiveListNeed,
-    getFinalListNeed,
-    activeListNeed,
-    finalListNeed
-  }
+        allListNeed.value = data.value;
+    }
+
+    const getPersonById = async (id: number): Promise<any> => useApi('/help-one', {
+        method: "GET",
+        headers: {
+            'Accept-Language': locale.value,
+        },
+        query: {
+            id
+        }
+    })
+
+    return {
+        getPersonById,
+        getActiveListNeed,
+        getFinalListNeed,
+        getAllListNeed,
+        activeListNeed,
+        finalListNeed,
+        allListNeed
+    }
 })
